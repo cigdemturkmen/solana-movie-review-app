fe6e1fd8d910311e88ba35f7f96378d3f8dab9ed
Cigdem Turkmen <cigdemturkmen57@outlook.com>
2022-12-24 23:10:54 +0300

    instruction schema and submit function are created

--- components/Form.tsx
+++ components/Form.tsx
@@ -2,13 +2,17 @@ import { FC } from 'react';
 import { Movie } from '../models/Movie';
 import { useState } from 'react';
 import { Box, Button, FormControl, FormLabel, Input, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Textarea } from '@chakra-ui/react';
+import * as web3 from '@solana/web3.js'
+import { useConnection, useWallet } from '@solana/wallet-adapter-react'
 
-const MOVIE_REVIEW_PROGRAM_ID = 'CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN'
+const MOVIE_REVIEW_PROGRAM_ID = 'CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN' 
 
 export const Form: FC = () => {
     const [title, setTitle] = useState('')
     const [rating, setRating] = useState(0)
     const [message, setMessage] = useState('')
+    const { connection } = useConnection(); // sets up an RPC connection
+	const { publicKey, sendTransaction } = useWallet(); // gets wallet details
 
     const handleSubmit = (event: any) => {
         event.preventDefault()
@@ -17,7 +21,56 @@ export const Form: FC = () => {
     }
 
     const handleTransactionSubmit = async (movie: Movie) => {
-        console.log(JSON.stringify(movie))
+        if(!publicKey) { // you can leave review only if you connect your wallet
+            alert('Please connect your wallet');
+            return;
+        }
+
+        const transaction = new web3.Transaction();
+        const buffer = movie.serialize();
+
+        // submitting data for storage on the network, will create an account to store it(to store our movie rewiev). These are PDAs(program derived addresses)
+        // we need the account address to send the transaction but PDAs require a transaction to be created. !! the solution is...?
+        const [pda] = await web3.PublicKey.findProgramAddress(
+            [publicKey.toBuffer(), new TextEncoder().encode(movie.title)],
+            new web3.PublicKey(MOVIE_REVIEW_PROGRAM_ID)
+        )
+
+        const instruction = new web3.TransactionInstruction({
+            keys: [
+                {
+                    // Your account will pay the fees, so it's writing to the network
+                    pubkey: publicKey,
+                    isSigner: true,
+                    isWritable: false,
+                },
+                {
+                    // The PDA will store the movie review 
+                    pubkey: pda,
+                    isSigner: false,
+                    isWritable: true
+                },
+                {
+                    // The system program will be used for creating the PDA
+                    pubkey: web3.SystemProgram.programId,
+                    isSigner: false,
+                    isWritable: false
+                }
+            ],
+            // Here's the most important part!
+            data: buffer,
+            programId: new web3.PublicKey(MOVIE_REVIEW_PROGRAM_ID)
+        })
+    
+        transaction.add(instruction)
+    
+        try {
+            let txid = await sendTransaction(transaction, connection)
+            console.log(`Transaction submitted: https://explorer.solana.com/tx/${txid}?cluster=devnet`)
+        } catch (e) {
+            alert(JSON.stringify(e))
+        }
+
     }
 
     return (
--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -23,3 +23,4 @@ export const MovieList: FC = () => {
         </div>
     )
 }
+
